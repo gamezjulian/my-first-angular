404bc67fa9c315dd35d1f2d53d439e9857038bb7
Julián Gamez <julian.gamez@brightstarr.com>
2017-01-20 22:53:49 -0300

    others

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ToPostsComponent } from './components/to-posts/to-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToPostsComponent,
+    ToPostsComponent
   ],
   imports: [
     BrowserModule,
